83dfd477e5f61edb6b67955d65c33b1f878b91fc
dGr8LookinSparky <hissparkyness@gmail.com>
2016-02-24 20:05:31 -0500

    Change to the Regular Dragoon: - Decrease the regular goon bite repeat to the Vanilla value of 700.
    Changes to the Advanced Dragoon:
    - Decrease the bite repeat to the Vanilla value of 600.
    - Decrease the barb repeat to the Vanilla value of 1000.
    - Decrease the barb regen to 12500.
    - Decrease the barb splash radius to 0.
    
    Changes to both Dragoons
    - Increase the base bite range to the Vanilla value of 96.
    - Increase the base pounce range to the Vanilla value of 72.
    - Increase the pounce width to the Vanilla value of 16.
    - Decrease the pounce charge time to the Vanilla value of 700.

--- src/game/tremulous.h
+++ src/game/tremulous.h
@@ -93,29 +93,29 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define LEVEL2_WALLJUMP_MAXSPEED    1000.0f
 
 #define LEVEL3_CLAW_DMG             ADM(80)
-#define LEVEL3_CLAW_RANGE           80.0f
-#define LEVEL3_CLAW_UPG_RANGE       LEVEL3_CLAW_RANGE + 3.0f          
+#define LEVEL3_CLAW_RANGE           96.0f
+#define LEVEL3_CLAW_UPG_RANGE       LEVEL3_CLAW_RANGE // + 3.0f          
 #define LEVEL3_CLAW_WIDTH           12.0f
-#define LEVEL3_CLAW_REPEAT          900
+#define LEVEL3_CLAW_REPEAT          700
 #define LEVEL3_CLAW_K_SCALE         1.0f
-#define LEVEL3_CLAW_U_REPEAT        800
+#define LEVEL3_CLAW_U_REPEAT        600
 #define LEVEL3_CLAW_U_K_SCALE       1.0f
 #define LEVEL3_POUNCE_DMG           ADM(100)
-#define LEVEL3_POUNCE_RANGE         48.0f
+#define LEVEL3_POUNCE_RANGE         72.0f
 #define LEVEL3_POUNCE_UPG_RANGE     LEVEL3_POUNCE_RANGE + 3.0f
-#define LEVEL3_POUNCE_WIDTH         14.0f
-#define LEVEL3_POUNCE_TIME          800      // msec for full Dragoon pounce
-#define LEVEL3_POUNCE_TIME_UPG      800      // msec for full Adv. Dragoon pounce
+#define LEVEL3_POUNCE_WIDTH         16.0f
+#define LEVEL3_POUNCE_TIME          700      // msec for full Dragoon pounce
+#define LEVEL3_POUNCE_TIME_UPG      700      // msec for full Adv. Dragoon pounce
 #define LEVEL3_POUNCE_TIME_MIN      200      // msec before which pounce cancels  
 #define LEVEL3_POUNCE_REPEAT        400      // msec before a new pounce starts
 #define LEVEL3_POUNCE_SPEED_MOD     0.75f    // walking speed modifier for pounce charging
 #define LEVEL3_POUNCE_JUMP_MAG      700      // Dragoon pounce jump power
 #define LEVEL3_POUNCE_JUMP_MAG_UPG  800      // Adv. Dragoon pounce jump power
 #define LEVEL3_BOUNCEBALL_DMG       ADM(110)
-#define LEVEL3_BOUNCEBALL_REPEAT    1200
+#define LEVEL3_BOUNCEBALL_REPEAT    1000
 #define LEVEL3_BOUNCEBALL_SPEED     1000.0f
-#define LEVEL3_BOUNCEBALL_RADIUS    75
-#define LEVEL3_BOUNCEBALL_REGEN     15000    // msec until new barb
+#define LEVEL3_BOUNCEBALL_RADIUS    0
+#define LEVEL3_BOUNCEBALL_REGEN     12500    // msec until new barb
 
 #define LEVEL4_CLAW_DMG             ADM(100)
 #define LEVEL4_CLAW_RANGE           100.0f
