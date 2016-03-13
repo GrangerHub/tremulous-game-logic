21455d2a8e52f3fe77fe808887ac39d22111c7ef
Cengiz Gunay <cengique@users.sf.net>
2016-03-13 17:28:23 -0400

    Made FS_READ syscall to return actual bytes read
    Haven't used it, but it may be useful in the future.

--- src/cgame/cg_local.h
+++ src/cgame/cg_local.h
@@ -1973,7 +1973,7 @@ void          trap_LiteralArgs( char *buffer, int bufferLength );
 // filesystem access
 // returns length of file
 int           trap_FS_FOpenFile( const char *qpath, fileHandle_t *f, fsMode_t mode );
-void          trap_FS_Read( void *buffer, int len, fileHandle_t f );
+int           trap_FS_Read( void *buffer, int len, fileHandle_t f );
 void          trap_FS_Write( const void *buffer, int len, fileHandle_t f );
 void          trap_FS_FCloseFile( fileHandle_t f );
 void          trap_FS_Seek( fileHandle_t f, long offset, fsOrigin_t origin ); // fsOrigin_t
--- src/game/g_local.h
+++ src/game/g_local.h
@@ -1216,7 +1216,7 @@ int       trap_Argc( void );
 void      trap_Argv( int n, char *buffer, int bufferLength );
 void      trap_Args( char *buffer, int bufferLength );
 int       trap_FS_FOpenFile( const char *qpath, fileHandle_t *f, fsMode_t mode );
-void      trap_FS_Read( void *buffer, int len, fileHandle_t f );
+int       trap_FS_Read( void *buffer, int len, fileHandle_t f );
 void      trap_FS_Write( const void *buffer, int len, fileHandle_t f );
 void      trap_FS_FCloseFile( fileHandle_t f );
 int       trap_FS_GetFileList( const char *path, const char *extension, char *listbuf, int bufsize );
