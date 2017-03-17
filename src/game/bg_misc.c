ce0166e9948179f7976a3ddb28a6f84e256d094d
dGr8LookinSparky <hissparkyness@gmail.com>
2017-03-17 14:16:47 -0400

    Fix the existing evolution tree code so that it works in warmup.

--- src/game/bg_misc.c
+++ src/game/bg_misc.c
@@ -1546,17 +1546,7 @@ int BG_ClassCanEvolveFromTo( class_t fclass,
   int i, j, best, value;
 
   if( gameIsInWarmup )
-  {
-    if( tclass == PCL_HUMAN ||
-        tclass == PCL_HUMAN_BSUIT ||
-        tclass == PCL_ALIEN_BUILDER0 )
-      return -1;
-
-    if (tclass <= fclass)
-      return -1;
-
-    return 0;
-  }
+    credits = ALIEN_MAX_CREDITS;
 
 
 
@@ -1591,7 +1581,7 @@ int BG_ClassCanEvolveFromTo( class_t fclass,
         best = value;
     }
 
-    return best <= credits ? best : -1;
+    return best <= credits ? (gameIsInWarmup ? 0 : best) : -1;
   }
 
   Com_Printf( S_COLOR_YELLOW "WARNING: fallthrough in BG_ClassCanEvolveFromTo\n" );
