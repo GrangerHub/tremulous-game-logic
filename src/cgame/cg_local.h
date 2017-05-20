ba4d106a97431fd322bff03c9047bd88ef35f8db
dGr8LookinSparky <hissparkyness@gmail.com>
2017-05-20 04:46:27 -0400

    Seperate the server entrance sound from the general chat sound.

--- src/cgame/cg_local.h
+++ src/cgame/cg_local.h
@@ -1235,6 +1235,9 @@ typedef struct
   qhandle_t   binaryAlpha1Shader;
   cgMediaBinaryShader_t binaryShaders[ NUM_BINARY_SHADERS ];
 
+  // enter
+  qhandle_t   enterSound;
+
   // disconnect
   qhandle_t   disconnectPS;
   qhandle_t   disconnectSound;
