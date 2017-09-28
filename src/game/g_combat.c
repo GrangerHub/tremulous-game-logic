f8f28f6e5a82d62fc967956fb3d025d90b1058a7
dGr8LookinSparky <hissparkyness@gmail.com>
2017-09-28 18:44:13 -0400

    Fix a segfault in G_Damage().

--- src/game/g_combat.c
+++ src/game/g_combat.c
@@ -1241,6 +1241,7 @@ void G_Damage( gentity_t *targ, gentity_t *inflictor, gentity_t *attacker,
 
   // Hack to ensure that old maps with trigger hurt meant to instantly kill, does so to the new bsuits.
   if( mod == MOD_TRIGGER_HURT &&
+      targ->client &&
       BG_InventoryContainsUpgrade( UP_BATTLESUIT, targ->client->ps.stats ) &&
     damage >= 400 &&
     damage < 10000 )
