1ca647011285dba40287ebd61990c3c75f85a40d
dGr8LookinSparky <hissparkyness@gmail.com>
2018-01-13 14:19:05 -0500

    - Decrease GIB_HEALTH to -100 hp. - Fix gibbing between player death and corpse spawn.

--- src/game/g_combat.c
+++ src/game/g_combat.c
@@ -287,6 +287,8 @@ void GibEntity( gentity_t *self )
   {
     self->s.otherEntityNum2 |= CLF_GIBBED;
   }
+
+  self->die = NULL;
 }
 
 /*
@@ -447,11 +449,13 @@ void player_die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int
     {
       self->takedamage = qtrue; // can still be gibbed
       G_SetContents( self, CONTENTS_CORPSE );
+      self->die = body_die;
     }
   } else
   {
     self->takedamage = qfalse;
     G_SetContents( self, 0 );
+    self->die = NULL;
   }
   
 
