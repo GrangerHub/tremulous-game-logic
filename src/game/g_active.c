80963b29e079761af99fe71a6c8255b3196ca71b
Darren <dkameoka@gmail.com>
2019-07-15 21:19:44 -1000

    OVERHAUL WIP: Remove snap entity vectors. Fixes glitchy player on player movement

--- src/game/g_active.c
+++ src/game/g_active.c
@@ -1187,7 +1187,7 @@ void SendPendingPredictableEvents( playerState_t *ps )
     // create temporary entity for event
     t = G_TempEntity( ps->origin, event );
     number = t->s.number;
-    BG_PlayerStateToEntityState( ps, &t->s, qtrue );
+    BG_PlayerStateToEntityState( ps, &t->s );
     t->s.number = number;
     t->s.eType = ET_EVENTS + event;
     t->s.eFlags |= EF_PLAYER_EVENT;
@@ -2276,10 +2276,9 @@ void ClientThink_real( gentity_t *ent )
   if( g_smoothClients.integer )
     BG_PlayerStateToEntityStateExtraPolate( &ent->client->ps,
                                             &ent->s,
-                                            ent->client->ps.commandTime,
-                                            qtrue );
+                                            ent->client->ps.commandTime );
   else
-    BG_PlayerStateToEntityState( &ent->client->ps, &ent->s, qtrue );
+    BG_PlayerStateToEntityState( &ent->client->ps, &ent->s );
 
   switch( client->ps.weapon )
   {
@@ -2556,9 +2555,9 @@ void ClientEndFrame( gentity_t *ent )
   // set the latest infor
   if( g_smoothClients.integer )
     BG_PlayerStateToEntityStateExtraPolate( &ent->client->ps, &ent->s,
-                                            ent->client->ps.commandTime, qtrue );
+                                            ent->client->ps.commandTime );
   else
-    BG_PlayerStateToEntityState( &ent->client->ps, &ent->s, qtrue );
+    BG_PlayerStateToEntityState( &ent->client->ps, &ent->s );
 
   SendPendingPredictableEvents( &ent->client->ps );
 }
